f3ce4af1cad4b884e97a04a34f8853313256842d
Jeff Bush <jeffbush001@gmail.com>
2016-08-18 20:30:18 -0700

    Flesh out NetTreeModelTest

--- src/test/java/NetTreeModelTest.java
+++ src/test/java/NetTreeModelTest.java
@@ -23,17 +23,57 @@ public class NetTreeModelTest
     {
         NetTreeModel model = new NetTreeModel();
         model.enterScope("scope1");
-        model.addNet("child1", 1);
-        model.addNet("child2", 2);
+        model.addNet("child1", 17);
+        model.addNet("child2", 19);
         model.enterScope("scope2");
-        model.addNet("child3", 3);
-        model.addNet("child4", 4);
+        model.addNet("child3", 23);
+        model.addNet("child4", 27);
         model.leaveScope();
-        model.addNet("child5", 5);
+        model.addNet("child5", 31);
 
         Object root = model.getRoot();
         assertEquals(4, model.getChildCount(root));
+        Object kid0 = model.getChild(root, 0);
+        Object kid1 = model.getChild(root, 1);
+        Object kid2 = model.getChild(root, 2);
+        Object kid3 = model.getChild(root, 3);
 
-        // ...
+        Object grandkid0 = model.getChild(kid2, 0);
+        Object grandkid1 = model.getChild(kid2, 1);
+
+        assertEquals("child1", kid0.toString());
+        assertEquals(0, model.getChildCount(kid0));
+        assertTrue(model.isLeaf(kid0));
+        assertEquals(0, model.getIndexOfChild(root, kid0));
+        assertEquals(17, model.getNetFromTreeObject(kid0));
+
+        assertEquals("child2", kid1.toString());
+        assertEquals(0, model.getChildCount(kid1));
+        assertTrue(model.isLeaf(kid1));
+        assertEquals(1, model.getIndexOfChild(root, kid1));
+        assertEquals(19, model.getNetFromTreeObject(kid1));
+
+        assertEquals("scope2", kid2.toString());
+        assertEquals(2, model.getChildCount(kid2));
+        assertFalse(model.isLeaf(kid2));
+        assertEquals(2, model.getIndexOfChild(root, kid2));
+
+        assertEquals("child5", kid3.toString());
+        assertEquals(0, model.getChildCount(kid3));
+        assertTrue(model.isLeaf(kid3));
+        assertEquals(3, model.getIndexOfChild(root, kid3));
+        assertEquals(31, model.getNetFromTreeObject(kid3));
+
+        assertEquals("child3", grandkid0.toString());
+        assertEquals(0, model.getChildCount(grandkid0));
+        assertTrue(model.isLeaf(grandkid0));
+        assertEquals(0, model.getIndexOfChild(kid2, grandkid0));
+        assertEquals(23, model.getNetFromTreeObject(grandkid0));
+
+        assertEquals("child4", grandkid1.toString());
+        assertEquals(0, model.getChildCount(grandkid1));
+        assertTrue(model.isLeaf(grandkid1));
+        assertEquals(1, model.getIndexOfChild(kid2, grandkid1));
+        assertEquals(27, model.getNetFromTreeObject(grandkid1));
     }
 }
