d2d9ad8ab2b4deb9e4e4a11e608c6b3281db899b
Jeff Bush <jeffbush001@gmail.com>
2018-07-06 14:02:28 -0700

    Move ExpectTraceBuilder and DummyTraceBuilder out of VCDLoaderTest
    These are independent of the VCD format and could be reused for other format tests.

--- src/test/java/VCDLoaderTest.java
+++ src/test/java/VCDLoaderTest.java
@@ -1,4 +1,3 @@
-
 //
 // Copyright 2016 Jeff Bush
 //
@@ -27,7 +26,6 @@ import java.util.ArrayList;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import waveview.BitVector;
 import waveview.TraceBuilder;
 import waveview.TraceLoader;
 import waveview.VCDLoader;
@@ -35,158 +33,6 @@ import waveview.VCDLoader;
 public class VCDLoaderTest {
     @Rule
     public final TemporaryFolder fTempFolder = new TemporaryFolder();
-
-    // Implements TraceBuilder interface, which the VCDLoader will write into,
-    // but asserts if events don't match a pre-defined sequence.
-    static class ExpectTraceBuilder implements TraceBuilder {
-        static final int EXPECT_ENTER = 0;
-        static final int EXPECT_EXIT = 1;
-        static final int EXPECT_NET = 2;
-        static final int EXPECT_TRANSITION = 3;
-        static final int EXPECT_FINISHED = 4;
-        static final int EXPECT_TIMESCALE = 5;
-
-        private ArrayList<Event> eventList = new ArrayList<>();
-        private int currentEventIndex;
-        private int nextNetId;
-
-        static class Event {
-            int type;
-            String name;
-            int id;
-            int width;
-            long timestamp;
-            BitVector values;
-
-            Event(int type) {
-                this.type = type;
-            }
-        }
-
-        @Override
-        public void setTimescale(int order) {
-            System.out.println("setTimescale " + order);
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_TIMESCALE);
-            assertEquals(event.timestamp, (long) order);
-        }
-
-        @Override
-        public void enterScope(String name) {
-            System.out.println("enterScope " + name);
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_ENTER);
-        }
-
-        @Override
-        public void exitScope() {
-            System.out.println("exitScope");
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_EXIT);
-        }
-
-        @Override
-        public int newNet(String shortName, int cloneId, int width) {
-            System.out.println("newNet " + shortName + " " + cloneId + " " + width);
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_NET);
-            assertEquals(event.name, shortName);
-            assertEquals(event.id, cloneId);
-            assertEquals(event.width, width);
-
-            return nextNetId++;
-        }
-
-        @Override
-        public void appendTransition(int id, long timestamp, BitVector values) {
-            System.out.println("appendTransition " + id + " " + timestamp + " " + values.toString(2));
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_TRANSITION);
-            assertEquals(event.timestamp, timestamp);
-
-            // Convert to string instead of using compare so Z and X values are
-            // handled correctly.
-            assertEquals(event.values.toString(2), values.toString(2));
-        }
-
-        @Override
-        public void loadFinished() {
-            System.out.println("loadFinished");
-
-            Event event = eventList.get(currentEventIndex++);
-            assertEquals(event.type, EXPECT_FINISHED);
-        }
-
-        void expectTimescale(int order) {
-            Event event = new Event(EXPECT_TIMESCALE);
-            event.timestamp = order;
-            eventList.add(event);
-        }
-
-        void expectEnterScope(String name) {
-            Event event = new Event(EXPECT_ENTER);
-            event.name = name;
-            eventList.add(event);
-        }
-
-        void expectExitScope() {
-            eventList.add(new Event(EXPECT_EXIT));
-        }
-
-        void expectNewNet(String name, int cloneId, int width) {
-            Event event = new Event(EXPECT_NET);
-            event.name = name;
-            event.id = cloneId;
-            event.width = width;
-            eventList.add(event);
-        }
-
-        void expectAppendTransition(int id, long timestamp, String bitString) {
-            Event event = new Event(EXPECT_TRANSITION);
-            event.id = id;
-            event.timestamp = timestamp;
-            event.values = new BitVector(bitString, 2);
-            eventList.add(event);
-        }
-
-        void expectLoadFinished() {
-            eventList.add(new Event(EXPECT_FINISHED));
-        }
-    }
-
-    static class DummyTraceBuilder implements TraceBuilder {
-        private int nextNetId;
-
-        @Override
-        public void setTimescale(int order) {
-        }
-
-        @Override
-        public void enterScope(String name) {
-        }
-
-        @Override
-        public void exitScope() {
-        }
-
-        @Override
-        public int newNet(String shortName, int cloneId, int width) {
-            return nextNetId++;
-        }
-
-        @Override
-        public void appendTransition(int id, long timestamp, BitVector values) {
-        }
-
-        @Override
-        public void loadFinished() {
-        }
-    }
 
     // Simulataneously builds VCD file contents and populates the
     // ExpectTraceBuilder with matching events
