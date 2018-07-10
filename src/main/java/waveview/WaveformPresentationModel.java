jbush001/WaveView

commit 4b44e51d33c8d390ce8b7d4f1f6ba45bc746bfcc

Get rid of WaveformPresentationModel.setSelectionStart
There is now a flag in setCursorPosition that indicates if the selection should
be extended, and all logic is consolidated in setCursorPosition.

src/main/java/waveview/WaveformPresentationModel.java
@@ -207,14 +207,22 @@ public class WaveformPresentationModel {
         return cursorPosition;
     }
 
-    public void setCursorPosition(long timestamp) {
+    public void setCursorPosition(long timestamp, boolean extendSelection) {
         long old = cursorPosition;
         cursorPosition = timestamp;
+        if (!extendSelection) {
+            selectionStart = timestamp;
+        }
+
         if (old != timestamp) {
             for (Listener listener : listeners) {
                 listener.cursorChanged(old, timestamp);
             }
         }
+    }
+
+    public void setCursorPosition(long timestamp) {
+        setCursorPosition(timestamp, false);
     }
 
     // This is used to display the timestamp at the top of the cursor when the user
@@ -235,10 +243,6 @@ public class WaveformPresentationModel {
 
     public long getSelectionStart() {
         return selectionStart;
-    }
-
-    public void setSelectionStart(long timestamp) {
-        selectionStart = timestamp;
     }
 
     public void removeAllMarkers() {
@@ -332,10 +336,7 @@ public class WaveformPresentationModel {
             timestamp = getTimestampForMarker(id);
         }
 
-        setCursorPosition(timestamp);
-        if (!extendSelection) {
-            setSelectionStart(timestamp);
-        }
+        setCursorPosition(timestamp, extendSelection);
     }
 
     public void nextMarker(boolean extendSelection) {
@@ -347,11 +348,13 @@ public class WaveformPresentationModel {
                 timestamp = getTimestampForMarker(id);
             }
 
-            setCursorPosition(timestamp);
-            if (!extendSelection) {
-                setSelectionStart(timestamp);
-            }
+            setCursorPosition(timestamp, extendSelection);
         }
+    }
+
+    public void jumpToMarker(int markerId, boolean extendSelection) {
+        long timestamp = getTimestampForMarker(markerId);
+        setCursorPosition(timestamp, extendSelection);
     }
 
     private static class Marker implements SortedArrayList.Keyed {

src/main/java/waveview/WaveformView.java
@@ -257,11 +257,8 @@ class WaveformView extends JPanel implements MouseListener, MouseMotionListener,
             repaint(); // we already had a selection, clear it
         }
 
-        if (!e.isShiftDown()) {
-            waveformPresentationModel.setSelectionStart(timestamp);
-        }
-
-        waveformPresentationModel.setCursorPosition(timestamp);
+        boolean extendSelection = e.isShiftDown();
+        waveformPresentationModel.setCursorPosition(timestamp, extendSelection);
 
         /// @bug Be sure to do this after setting the position, otherwise the view will
         /// jump back to the

src/main/java/waveview/MainWindow.java
@@ -343,11 +343,7 @@ public class MainWindow extends JPanel implements ActionListener {
         if (currentSearch != null) {
             long newTimestamp = currentSearch.getNextMatch(waveformPresentationModel.getCursorPosition());
             if (newTimestamp >= 0) {
-                if (!extendSelection) {
-                    waveformPresentationModel.setSelectionStart(newTimestamp);
-                }
-
-                waveformPresentationModel.setCursorPosition(newTimestamp);
+                waveformPresentationModel.setCursorPosition(newTimestamp, extendSelection);
             }
         }
     }
@@ -356,11 +352,7 @@ public class MainWindow extends JPanel implements ActionListener {
         if (currentSearch != null) {
             long newTimestamp = currentSearch.getPreviousMatch(waveformPresentationModel.getCursorPosition());
             if (newTimestamp >= 0) {
-                if (!extendSelection) {
-                    waveformPresentationModel.setSelectionStart(newTimestamp);
-                }
-
-                waveformPresentationModel.setCursorPosition(newTimestamp);
+                waveformPresentationModel.setCursorPosition(newTimestamp, extendSelection);
             }
         }
     }

src/main/java/waveview/MarkerListView.java
@@ -64,11 +64,7 @@ class MarkerListView extends JPanel implements ActionListener, WaveformPresentat
     }
 
     private void select(boolean extendSelection) {
-        long timestamp = waveformPresentationModel.getTimestampForMarker(table.getSelectedRow());
-        waveformPresentationModel.setCursorPosition(timestamp);
-        if (!extendSelection) {
-            waveformPresentationModel.setSelectionStart(timestamp);
-        }
+        waveformPresentationModel.jumpToMarker(table.getSelectedRow(), extendSelection);
     }
 
     @Override

src/test/java/MarkerTest.java
@@ -257,11 +257,10 @@ public class MarkerTest {
 
     // Extend the selection while navigating to next marker
     @Test
-    public void extendSelectionNextMarker() {
+    public void nextMarkerExtendSelection() {
         model.addMarker("marker1", 100);
         model.addMarker("marker2", 200);
         model.setCursorPosition(50);
-        model.setSelectionStart(50);
         clearInvocations(listener);
 
         model.nextMarker(true);
@@ -274,11 +273,10 @@ public class MarkerTest {
 
     // Extend the selection while navigating to previous marker
     @Test
-    public void extendSelectionPrevMarker() {
+    public void prevMarkerExtendSelection() {
         model.addMarker("marker2", 100);
         model.addMarker("marker3", 200);
         model.setCursorPosition(150);
-        model.setSelectionStart(150);
         clearInvocations(listener);
 
         model.prevMarker(true);
@@ -287,6 +285,34 @@ public class MarkerTest {
         verifyNoMoreInteractions(listener);
         assertEquals(100, model.getCursorPosition());
         assertEquals(150, model.getSelectionStart());
+    }
+
+    @Test
+    public void jumpToMarker() {
+        model.addMarker("marker", 100);
+        model.setCursorPosition(50);
+        clearInvocations(listener);
+
+        model.jumpToMarker(0, false);
+
+        verify(listener).cursorChanged(50, 100);
+        verifyNoMoreInteractions(listener);
+        assertEquals(100, model.getCursorPosition());
+        assertEquals(100, model.getSelectionStart());
+    }
+
+    @Test
+    public void jumpToMarkerExtendSelection() {
+        model.addMarker("marker", 100);
+        model.setCursorPosition(50);
+        clearInvocations(listener);
+
+        model.jumpToMarker(0, true);
+
+        verify(listener).cursorChanged(50, 100);
+        verifyNoMoreInteractions(listener);
+        assertEquals(100, model.getCursorPosition());
+        assertEquals(50, model.getSelectionStart());
     }
 
     @Test

commit 37c49dadc54a91ca05117c9e1ec75c39d9f6c346

Fix issue introduced in 4b44e51d33c8d390ce8b7d4f1f6ba45bc746bfcc
Range selection was broken because the beginning of the selection jumped to the current
cursor position. This was because setAdjustingCursor() was calling setCursorPosition()
as a side effect to send a notification. I made it send the notification directly to
avoid messing up the selection. Using the cursor changed notification is still a bit hacky.

src/main/java/waveview/WaveformPresentationModel.java
@@ -230,11 +230,10 @@ public class WaveformPresentationModel {
     public void setAdjustingCursor(boolean adjust) {
         adjustingCursor = adjust;
 
-        /// @bug This is a hacky way to force everyone to update, but has odd
-        /// side effects if it is done in the wrong order. There should most
-        /// likely be another way to do this, like, for example, another event
-        /// to notify clients that the cursor is in the adjusting state.
-        setCursorPosition(cursorPosition);
+        // This forces views to update
+        for (Listener listener : listeners) {
+            listener.cursorChanged(cursorPosition, cursorPosition);
+        }
     }
 
     public boolean isAdjustingCursor() {

src/main/java/waveview/WaveformView.java
@@ -260,11 +260,9 @@ class WaveformView extends JPanel implements MouseListener, MouseMotionListener,
         boolean extendSelection = e.isShiftDown();
         waveformPresentationModel.setCursorPosition(timestamp, extendSelection);
 
-        /// @bug Be sure to do this after setting the position, otherwise the view will
-        /// jump back to the
-        /// old cursor position first and invoke the auto-scroll-to logic. This is
-        /// pretty clunky, and
-        /// should probably be rethought.
+        // Do this after setting cursor position, otherwise it will jump to the old
+        // cursor position (as it send a cursor position update as a hacky way to
+        // force the timestamp view to update).
         waveformPresentationModel.setAdjustingCursor(true);
     }
 
@@ -276,7 +274,8 @@ class WaveformView extends JPanel implements MouseListener, MouseMotionListener,
     @Override
     public void mouseDragged(MouseEvent e) {
         long timestamp = xCoordinateToTimestamp(e.getX());
-        waveformPresentationModel.setCursorPosition(timestamp);
+        boolean extendSelection = e.isShiftDown();
+        waveformPresentationModel.setCursorPosition(timestamp, extendSelection);
 
         // Drag scrolling
         Rectangle r = new Rectangle(e.getX(), e.getY(), 1, 1);

src/test/java/WaveformPresentationModelTest.java
@@ -253,7 +253,11 @@ public class WaveformPresentationModelTest {
     public void adjustingCursor() {
         model.setAdjustingCursor(true);
         assertTrue(model.isAdjustingCursor());
+        verify(listener).cursorChanged(0, 0);
+        clearInvocations(listener);
+
         model.setAdjustingCursor(false);
         assertFalse(model.isAdjustingCursor());
+        verify(listener).cursorChanged(0, 0);
     }
 }