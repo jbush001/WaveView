35c162a0188ee3aa54431664fc6fcc8a0d8c137a
Jeff Bush <jeffbush001@gmail.com>
2018-07-28 15:46:29 -0700

    Forgot to add file in cbb31785d491afe6a16c1bb06674b1214256853e

---
+++ src/test/java/BitValueTest.java
@@ -0,0 +1,77 @@
+//
+// Copyright 2016 Jeff Bush
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import static org.junit.Assert.assertEquals;
+import waveview.BitValue;
+import org.junit.Test;
+
+public class BitValueTest {
+    @Test
+    public void fromChar() {
+        assertEquals(BitValue.ZERO, BitValue.fromChar('0'));
+        assertEquals(BitValue.ONE, BitValue.fromChar('1'));
+        assertEquals(BitValue.X, BitValue.fromChar('x'));
+        assertEquals(BitValue.X, BitValue.fromChar('X'));
+        assertEquals(BitValue.Z, BitValue.fromChar('z'));
+        assertEquals(BitValue.Z, BitValue.fromChar('Z'));
+    }
+
+    @Test
+    public void fromInt() {
+        assertEquals(BitValue.ZERO, BitValue.fromInt(0));
+        assertEquals(BitValue.ONE, BitValue.fromInt(1));
+        assertEquals(BitValue.ONE, BitValue.fromInt(213));
+    }
+
+    @Test
+    public void fromOrdinal() {
+        assertEquals(BitValue.ZERO, BitValue.fromOrdinal(BitValue.ZERO.ordinal()));
+        assertEquals(BitValue.ONE, BitValue.fromOrdinal(BitValue.ONE.ordinal()));
+        assertEquals(BitValue.X, BitValue.fromOrdinal(BitValue.X.ordinal()));
+        assertEquals(BitValue.Z, BitValue.fromOrdinal(BitValue.Z.ordinal()));
+    }
+
+    @Test
+    public void compare() {
+        assertEquals(0, BitValue.ZERO.compare(BitValue.ZERO));
+        assertEquals(1, BitValue.ONE.compare(BitValue.ZERO));
+        assertEquals(0, BitValue.X.compare(BitValue.ZERO));
+        assertEquals(0, BitValue.Z.compare(BitValue.ZERO));
+
+        assertEquals(-1, BitValue.ZERO.compare(BitValue.ONE));
+        assertEquals(0, BitValue.ONE.compare(BitValue.ONE));
+        assertEquals(0, BitValue.X.compare(BitValue.ONE));
+        assertEquals(0, BitValue.Z.compare(BitValue.ONE));
+
+        assertEquals(0, BitValue.ZERO.compare(BitValue.X));
+        assertEquals(0, BitValue.ONE.compare(BitValue.X));
+        assertEquals(0, BitValue.X.compare(BitValue.X));
+        assertEquals(0, BitValue.Z.compare(BitValue.X));
+
+        assertEquals(0, BitValue.ZERO.compare(BitValue.Z));
+        assertEquals(0, BitValue.ONE.compare(BitValue.Z));
+        assertEquals(0, BitValue.X.compare(BitValue.Z));
+        assertEquals(0, BitValue.Z.compare(BitValue.Z));
+    }
+
+    @Test
+    public void toChar() {
+        assertEquals('0', BitValue.ZERO.toChar());
+        assertEquals('1', BitValue.ONE.toChar());
+        assertEquals('x', BitValue.X.toChar());
+        assertEquals('z', BitValue.Z.toChar());
+    }
+}
